fdd54b8d27ae23d04b3b792f9a53a6ac131a7a02
Wynand van Rensburg <wkvanrensburg@gmail.com>
2023-04-13 17:32:58 +0200

    Add scaffold and appBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 //main() starting point for projects
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
       ),
     ),
   );
