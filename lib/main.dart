3ba22e432ea4af9394f9d20bd02d92a7540916aa
Wynand van Rensburg <wkvanrensburg@gmail.com>
2023-04-13 17:47:11 +0200

    Add image to body

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: const Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://www.freecodecamp.org/news/content/images/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg',
+            ),
+          ),
         ),
       ),
     ),
